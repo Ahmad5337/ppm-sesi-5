c2c8ebf7c038e758e3098bbc1c08460d68a210b6
Ahmad5337 <ghifariahma234@gmail.com>
2025-10-27 23:21:31 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigator from "./src/navigation/TabNavigator";
+import DetailScreen from "./src/screens/DetailScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Tabs" component={TabNavigator} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
